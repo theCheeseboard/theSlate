3c4b2b485ccb40126ae5f30816f62176b211b681
Victor Tran <vicr12345@gmail.com>
2018-06-13 22:05:09 +1000

    Update Travis CI instructions

--- travis.sh
+++ travis.sh
@@ -1,14 +1,45 @@
-
+if [ $STAGE = "script" ]; then
+  if [ $TRAVIS_OS_NAME = "linux" ]; then
+    echo "[TRAVIS] Preparing build environment"
+    source /opt/qt510/bin/qt510-env.sh
+    echo "[TRAVIS] Building and installing the-libs
     git clone https://github.com/vicr123/the-libs.git
     cd the-libs
     qmake
     make
     sudo make install INSTALL_ROOT=/
     cd ..
+    echo "[TRAVIS] Running qmake"
     qmake
+    echo "[TRAVIS] Building project"
     make
+    echo "[TRAVIS] Installing into appdir"
     make install INSTALL_ROOT=appdir
+    echo "[TRAVIS] Getting linuxdeployqt"
     wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
     chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+    echo "[TRAVIS] Building AppImage"
     "./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs"
     "./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage"
+  else
+    echo "[TRAVIS] Building for macOS"
+  fi
+elif [ $STAGE = "before_install" ]; then
+  if [ $TRAVIS_OS_NAME = "linux" ]; then
+    echo "[TRAVIS] Installing required dependencies"
+    wget -O ~/vicr12345.gpg.key https://vicr123.com/repo/apt/vicr12345.gpg.key
+    sudo apt-key add ~/vicr12345.gpg.key
+    sudo add-apt-repository 'deb https://vicr123.com/repo/apt/ubuntu bionic main'
+    sudo apt-get update -qq
+    sudo apt-get install qt5-qmake libqt5x11extras5-dev qt5-default
+  else
+    echo "[TRAVIS] Preparing to build for macOS"
+  fi
+elif [ $STAGE = "after_success" ]; then
+  if [ $TRAVIS_OS_NAME = "linux" ]; then
+    echo "[TRAVIS] Publishing AppImage"
+    find appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
+    wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
+    bash upload.sh theSlate*.AppImage*
+  fi
+fi
