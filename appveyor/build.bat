8cb13e7b4138dbf987c5bb10b508f7c982d6d24d
Victor Tran <vicr12345@gmail.com>
2018-07-26 16:04:13 +1000

    Stop AppVeyor fron building continuous branch

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -1,3 +1,7 @@
+if "%APPVEYOR_REPO_TAG_NAME%"=="continuous" (
+    exit 1
+)
+
 set QTDIR=C:\Qt\5.11\msvc2017_64
 set PATH=%PATH%;%QTDIR%\bin
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
