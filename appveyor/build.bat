1989eb55994252c8d1da0fdb07d1374857d7cb3f
Victor Tran <vicr12345@gmail.com>
2019-04-01 11:22:25 +1100

    Update AppVeyor build

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -5,7 +5,7 @@ if "%APPVEYOR_REPO_TAG_NAME%"=="continuous" (
 )
 
 
-set QTDIR=C:\Qt\5.11\msvc2017_64
+set QTDIR=C:\Qt\5.12\msvc2017_64
 set PATH=%PATH%;%QTDIR%\bin
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
